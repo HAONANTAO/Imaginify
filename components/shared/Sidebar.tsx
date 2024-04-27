39a256781d9118c4419763f56299df7458a58313
HAONANTAO <873190199@qq.com>
2024-04-27 16:45:39 +1000

    sign in sidebar

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,7 +1,13 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { SignedIn } from "@clerk/nextjs";
+import { navLinks } from "@/constants/index";
+import { usePathname } from "next/navigation";
 const Sidebar = () => {
+  console.log(navLinks);
+  const pathname = usePathname();
   return (
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
@@ -14,7 +20,28 @@ const Sidebar = () => {
           />
         </Link>
 
-        
+        {/* if the user is signed in can see these parts */}
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.map((link) => {
+                // check active according to pathname
+                const isActive = link.route === pathname;
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}>
+                    {link.label}
+                  </li>
+                );
+              })}
+            </ul>
+          </SignedIn>
+        </nav>
       </div>
     </aside>
   );

1b1bac52fe52f7281a89d7a27d74364c3b7e9c8c
HAONANTAO <873190199@qq.com>
2024-04-27 16:28:38 +1000

    basic set up

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 https://github.com/adrianhajdin/ai_saas_app/blob/main/tailwind.config.ts
+
+clerk
+nextjs tailwindcss
+shacn-ui
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../../components/shared/Sidebar";
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      <Sidebar />
+      <div className="root-container">
+        <div className="wrapper"> {children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default layout;
